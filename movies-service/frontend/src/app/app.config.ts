76a0cbfae6ae727055a78feb5f9a9e8ed2337237
vetall7 <shapovalovvit228@gmail.com>
2024-12-29 21:05:49 +0100

    feat: add interceptor

--- movies-service/frontend/src/app/app.config.ts
+++ movies-service/frontend/src/app/app.config.ts
@@ -2,16 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideStore} from '@ngrx/store';
 import {authReducer} from './states/auth/auth.reducer';
 import {provideEffects} from '@ngrx/effects';
 import {AuthEffects} from './states/auth/auth.effect';
 import {JWT_OPTIONS, JwtHelperService} from '@auth0/angular-jwt';
+import {authTokenInterceptor} from './features/auth/interceptors/auth-token';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideHttpClient(withFetch()), provideAnimationsAsync(),
+    provideRouter(routes), provideHttpClient(withFetch(), withInterceptors([authTokenInterceptor])), provideAnimationsAsync(),
     provideStore({
       auth: authReducer
     }),
