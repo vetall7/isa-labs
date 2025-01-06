96c58047ef51d0df13b8ae1b04a8869693ef8a05
vetall7 <shapovalovvit228@gmail.com>
2025-01-06 11:02:15 +0100

    feat: add external databases

--- movies-service/genre/src/main/java/org/example/genre/initializer/DataInitializer.java
+++ movies-service/genre/src/main/java/org/example/genre/initializer/DataInitializer.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class DataInitializer implements InitializingBean {
     private final GenreService genreService;
